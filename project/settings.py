a87fdb2479a31786ed43beb8214a5ae980291a0c
BEKSI <thydcj@naver.com>
2022-05-17 23:20:08 +0900

    디버그 off

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v=4m)*m_qt=@(!&u@s63lmn_y@h2&&+2nr4_@ttz&@%pcreg*-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
