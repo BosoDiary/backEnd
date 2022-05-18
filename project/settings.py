7c4f1fdc7f3e554e023b6d6b2fa8df7f7d4d6a4c
김보석 <boseoggim08@gmail.com>
2022-05-18 18:00:33 +0900

    bosodiary 앱 생성

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v=4m)*m_qt=@(!&u@s63lmn_y@h2&&+2nr4_@ttz&@%pcreg*-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
