2b6b4c364e0677ce64bda69a084f84fd291d8ea4
김보석 <boseoggim08@gmail.com>
2022-05-31 09:32:27 +0900

    api url 생성 난 커밋 메시지 내 맘대로 한당git add .

--- bosodiary/views.py
+++ bosodiary/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+#from .models import Calc
+#from .serializers import CalcSerializer
+
+# Create your views here.
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response("Hello API!")
